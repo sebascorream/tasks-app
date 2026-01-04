6ff83490e9095f5c277166d990cccdf5767f6a88
sebascorream <sebascorreamorales@gmail.com>
2026-01-03 21:53:16 -0500

    add start script for production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import app from './app.js';
 import { connectDB } from './db.js';
 
+const PORT = process.env.PORT || 3000;
+
 connectDB();
-app.listen(3000);
-console.log('Server on port', 3000);
+
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT);
+});
